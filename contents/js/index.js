ac2d471b83b36a28a9561a692eea67dd91efc270
NguyenCaoThin <nguyenthin23102000@gmail.com>
2020-09-14 12:41:18 +0700

    creating part banner

--- contents/js/index.js
+++ contents/js/index.js
@@ -4,4 +4,26 @@ $(document).ready(function() {
 	}, function() {
 		$(this).find('.dropdown-content').slideUp(200);
 	});
+	$('.nav-item.dropdown').on('click',function() {
+		$(this).find('.dropdown-content').slideToggle(500);
+	});
+
+	var pos_nav = $('nav').offset().top;
+	$(window).scroll(function(event) {
+		var pos = $('html,body').scrollTop();
+		if(pos>pos_nav){
+			$('nav').css({
+				'opacity': '0.8',
+				'background-color': 'rgba(90, 84, 184, .7)'
+			});
+			$('.nav-link').addClass('color-white');
+		}
+		else{
+			$('nav').css({
+				'background-color':'white',	
+				'opacity' : '1'
+			});
+			$('.nav-link').removeClass('color-white');
+		}
+	});
 });
